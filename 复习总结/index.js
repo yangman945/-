221067aee55882f9c75ccd8dcb8c7bf4abfad13a
地主兄 <yangman945@163.com>
2019-10-01 01:35:40 +0800

    js执行机制

---
+++ 复习总结/index.js
@@ -0,0 +1,41 @@
+var url = require('url');
+require('http').createServer(function(req, res) {
+    console.log('1');
+setTimeout(function() {
+    console.log('2');
+    process.nextTick(function() {
+        console.log('3');
+    })
+    new Promise(function(resolve) {
+        console.log('4');
+        resolve();
+    }).then(function() {
+        console.log('5')
+    })
+})
+process.nextTick(function() {
+    console.log('6');
+})
+new Promise(function(resolve) {
+    console.log('7');
+    resolve();
+}).then(function() {
+    console.log('8')
+})
+
+setTimeout(function() {
+    console.log('9');
+    process.nextTick(function() {
+        console.log('10');
+    })
+    new Promise(function(resolve) {
+        console.log('11');
+        resolve();
+    }).then(function() {
+        console.log('12')
+    })
+})
+    res.writeHead(200)
+}).listen(8080, '127.0.0.1',function(){
+    console.log('启动服务，监听 127.0.0.1:8080');
+})
